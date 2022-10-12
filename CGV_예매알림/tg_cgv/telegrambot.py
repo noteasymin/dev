1a9e26ca4f01d4a5486410ee80f4b6c9fc0f23fd
noteasymin <noteasymin@gmail.com>
2022-10-12 15:06:52 +0900

    Delete telegrambot.py

--- CGV_예매알림/tg_cgv/telegrambot.py
+++
@@ -1,8 +0,0 @@
-import telegram
-
-bot = telegram.Bot(token = '5038045836:AAHlx2ieZK4LH5yhzPEqhO8LI9TasoMenpI')
-
-# for i in bot.getUpdates():
-#     print(i.message)
-
-bot.sendMessage(chat_id = 5000538977, text = "테스트 입니다.")
